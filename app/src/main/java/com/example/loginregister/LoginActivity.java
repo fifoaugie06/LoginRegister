10d1d30d87e14d8557277481410b7b858ad723e1
AUGIE-PC <fifoaugie06@gmail.com>
2020-03-12 14:02:42 +0700

    initial commit

--- app/src/main/java/com/example/loginregister/LoginActivity.java
+++ app/src/main/java/com/example/loginregister/LoginActivity.java
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
     Button btnRegisterIntent;
+    ImageButton btnLogin;
+    EditText edtNim;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +22,8 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         btnRegisterIntent = findViewById(R.id.btn_Register_Intent);
+        btnLogin = findViewById(R.id.btn_login);
+        edtNim = findViewById(R.id.edt_nim);
 
         getSupportActionBar().hide();
 
@@ -24,6 +32,13 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View v) {
                 startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
                 finish();
+            }
+        });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(LoginActivity.this, "Selamat Datang " + edtNim.getText().toString(), Toast.LENGTH_SHORT).show();
             }
         });
     }
--- app/src/main/java/com/example/loginregister/RegisterActivity.java
+++ app/src/main/java/com/example/loginregister/RegisterActivity.java
@@ -6,9 +6,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
     Button btnLoginIntent;
+    ImageButton btnRegister;
+    EditText edtNick;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +21,8 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
 
         btnLoginIntent = findViewById(R.id.btn_Login_Intent);
+        btnRegister = findViewById(R.id.btn_Register);
+        edtNick = findViewById(R.id.edt_nick);
 
         btnLoginIntent.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,6 +34,13 @@ public class RegisterActivity extends AppCompatActivity {
                         finish();
                     }
                 });
+            }
+        });
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(RegisterActivity.this, edtNick.getText() + " Didaftarkan", Toast.LENGTH_SHORT).show();
             }
         });
 
